f4decff847c3b3e860d88cb97f350ae2c492acc2
aficat <aficatrashid@gmail.com>
2022-05-12 16:24:43 +0800

    main page toggler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
-import MaterialUIComponents from "./views/MaterialUI/MaterialUIComponents";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import MaterialUIComponents from "./views/MaterialUI/MaterialUIComponents";
 import AntDesignComponents from "./views/AntDesign/AntDesignComponents";
 import BootstrapComponents from "./views/Bootstrap/BootstrapComponents";
 
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <Router>
         <nav>
-          <div class="nav-wrapper">
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <div class="navbar-fixed">
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
               <li>
                 <Link to="/">Home</Link>
               </li>
@@ -33,8 +33,57 @@ function App() {
               exact
               path="/"
               element={
-                <div class="section container">
+                <div class="section">
                   <h1>React Libraries Toggler</h1>
+                  <div class="row">
+                    <div class="col s12 m4 l4">
+                      <div class="card">
+                        <div class="card-image">
+                          <img src="images/mui.jpg" alt="mui" />
+                        </div>
+                        <div class="card-action">
+                          <a
+                            class="waves-effect waves-light btn teal lighten-4"
+                            href="/mui"
+                          >
+                            Material UI
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col s12 m4 l4">
+                      <div class="card">
+                        <div class="card-image">
+                          <img src="images/atd.jpg" alt="atd" />
+                          <span class="card-title">Ant Design</span>
+                        </div>
+                        <div class="card-action">
+                          <a
+                            class="waves-effect waves-light btn teal lighten-4"
+                            href="/ad"
+                          >
+                            Ant Design
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col s12 m4 l4">
+                      <div class="card">
+                        <div class="card-image">
+                          <img src="images/bootstrap.jpg" alt="bs" />
+                          <span class="card-title">Bootstrap</span>
+                        </div>
+                        <div class="card-action">
+                          <a
+                            class="waves-effect waves-light btn teal lighten-4"
+                            href="/bs"
+                          >
+                            Bootstrap
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               }
             />
